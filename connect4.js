fef8814a314fcdde1dcc5ed165ecb3291e175c8c
Ethan Kemprowski <ethank27@vt.edu>
2024-01-10 01:09:48 -0500

    Finished css i think

--- connect4.js
+++ connect4.js
@@ -24,7 +24,7 @@ function setGame() {
     // Top left is (0,0), bottom right is (5, 6)
     for (let r = 0; r < rows; r++) {
         let row = [];
-        for (let c = 0; c < rows; c++) {
+        for (let c = 0; c < columns; c++) {
             //JS
             row.push(' ');
 
@@ -32,11 +32,11 @@ function setGame() {
             // Gets the ids and we will update them in JS
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
-            tile.classList("tile");
+            tile.classList.add("tile");
             // <div id="0-0" class="tile"></div>
             /* This line appends this HTML code automatically
                 for us in the board tag */
-            document.getElementsId("board").append(tile); 
+            document.getElementById("board").append(tile); 
         }
         board.push(row);
     }
